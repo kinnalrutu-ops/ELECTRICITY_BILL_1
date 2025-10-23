5df53282fe31d52956e558a35b2cb516577a1ee0
kinnalrutu-ops <kinnalrutu@gmail.com>
2025-10-23 14:49:15 +0530

    Added 10% discount feature for bills above ₹1000

--- electricbill.py
+++ electricbill.py
@@ -1,4 +1,5 @@
-# Electricity Bill Calculator Program
+
+# Electricity Bill Calculator Program with Discount Feature
 
 units = int(input("Enter number of units consumed: "))
 rate_per_unit = 5
@@ -7,3 +8,12 @@ total_bill = units * rate_per_unit
 
 print("Units Consumed:", units)
 print("Total Bill: ₹", total_bill)
+
+if total_bill > 1000:
+    discount = total_bill * 0.10
+    final_bill = total_bill - discount
+    print("Discount Applied: ₹", discount)
+    print("Final Bill: ₹", final_bill)
+else:
+    print("No discount applied.")
+    print("Final Bill: ₹", total_bill)
